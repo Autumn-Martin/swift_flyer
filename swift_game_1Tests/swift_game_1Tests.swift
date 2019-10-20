04f88a36ffcbaadf48c5d127f9e5c9f7a85d1240
Autumn-Martin <autumnmartin.dev@gmail.com>
2019-10-19 18:33:20 -0600

    Initial Commit

---
+++ swift_game_1Tests/swift_game_1Tests.swift
@@ -0,0 +1,34 @@
+//
+//  swift_game_1Tests.swift
+//  swift_game_1Tests
+//
+//  Created by Autumn Martin on 10/19/19.
+//  Copyright © 2019 Autumn Martin. All rights reserved.
+//
+
+import XCTest
+@testable import swift_game_1
+
+class swift_game_1Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
